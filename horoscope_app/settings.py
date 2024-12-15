5e673918b1bdb03a7b5c04a3fa86b5ad19e87f21
595085 <595085eg@gmail.com>
2024-12-15 11:26:33 +0700

    add more ALLOWED HOST

--- horoscope_app/settings.py
+++ horoscope_app/settings.py
@@ -33,7 +33,8 @@ if not SECRET_KEY:
 DEBUG = False
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['.onrender.com', 'localhost']
+# ALLOWED_HOSTS = ['.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','.onrender.com', 'https://horoscope-app-ma6d.onrender.com']
 
 
 
